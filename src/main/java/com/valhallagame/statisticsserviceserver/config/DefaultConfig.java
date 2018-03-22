015df8fdfe08d4ad9d71bf111fad1fbbf03db4b2
Mio Engel <mioengel@gmail.com>
2018-03-22 17:50:16 +0100

    Added development profile

--- src/main/java/com/valhallagame/statisticsserviceserver/config/DefaultConfig.java
+++ src/main/java/com/valhallagame/statisticsserviceserver/config/DefaultConfig.java
@@ -5,22 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.valhallagame.characterserviceclient.CharacterServiceClient;
-import com.valhallagame.common.DefaultServicePortMappings;
 import com.valhallagame.personserviceclient.PersonServiceClient;
 
 @Configuration
-@Profile("development")
-public class DevConfig {
+@Profile("default")
+public class DefaultConfig {
 	@Bean
 	public CharacterServiceClient characterServiceClient() {
-		CharacterServiceClient
-				.init("http://dev-character-service:" + DefaultServicePortMappings.CHARACTER_SERVICE_PORT);
 		return CharacterServiceClient.get();
 	}
 
 	@Bean
 	public PersonServiceClient personServiceClient() {
-		PersonServiceClient.init("http://dev-person-service:" + DefaultServicePortMappings.PERSON_SERVICE_PORT);
 		return PersonServiceClient.get();
 	}
 }

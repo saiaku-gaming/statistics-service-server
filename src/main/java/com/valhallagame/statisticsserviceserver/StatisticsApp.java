db216a195865fc259c5da94c1f37282e579e30d3
phrozen <phrozen@teamsamst.com>
2018-10-20 21:02:12 +0200

    Changed from App to ServiceNameApp and some other small stuff

--- src/main/java/com/valhallagame/statisticsserviceserver/StatisticsApp.java
+++ src/main/java/com/valhallagame/statisticsserviceserver/StatisticsApp.java
@@ -1,10 +1,5 @@
 package com.valhallagame.statisticsserviceserver;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-
 import com.valhallagame.common.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,19 +11,17 @@ import org.springframework.context.annotation.Bean;
 import com.valhallagame.common.DefaultServicePortMappings;
 
 @SpringBootApplication
-public class App {
+public class StatisticsApp {
 
-	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	private static final Logger logger = LoggerFactory.getLogger(StatisticsApp.class);
 
 	public static void main(String[] args) {
 		Properties.load(args, logger);
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(StatisticsApp.class, args);
 	}
 
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
-		return (container -> {
-			container.setPort(DefaultServicePortMappings.STATISTICS_SERVICE_PORT);
-		});
+		return (container -> container.setPort(DefaultServicePortMappings.STATISTICS_SERVICE_PORT));
 	}
 }

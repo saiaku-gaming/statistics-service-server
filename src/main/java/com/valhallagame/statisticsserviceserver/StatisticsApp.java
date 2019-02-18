60f9031ced71d2f5e0f7b2027c6c6e1e81bf324f
Mio Engel <mioengel@gmail.com>
2019-02-18 11:04:23 +0100

    Adds logging

--- src/main/java/com/valhallagame/statisticsserviceserver/StatisticsApp.java
+++ src/main/java/com/valhallagame/statisticsserviceserver/StatisticsApp.java
@@ -1,14 +1,17 @@
 package com.valhallagame.statisticsserviceserver;
 
+import com.valhallagame.common.DefaultServicePortMappings;
 import com.valhallagame.common.Properties;
+import com.valhallagame.common.filter.ServiceRequestFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import com.valhallagame.common.DefaultServicePortMappings;
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class StatisticsApp {
@@ -23,5 +26,23 @@ public class StatisticsApp {
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 		return (container -> container.setPort(DefaultServicePortMappings.STATISTICS_SERVICE_PORT));
+	}
+
+	@Bean
+	public FilterRegistrationBean serviceRequestFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(getServiceRequestFilter());
+		registration.addUrlPatterns(
+				"/*",
+				"/**"
+		);
+		registration.setName("serviceRequestFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+
+	@Bean(name = "serviceRequestFilter")
+	public Filter getServiceRequestFilter() {
+		return new ServiceRequestFilter();
 	}
 }
--- src/main/java/com/valhallagame/statisticsserviceserver/rabbitmq/NotificationConsumer.java
+++ src/main/java/com/valhallagame/statisticsserviceserver/rabbitmq/NotificationConsumer.java
@@ -1,18 +1,21 @@
 package com.valhallagame.statisticsserviceserver.rabbitmq;
 
+import com.valhallagame.common.rabbitmq.NotificationMessage;
+import com.valhallagame.statisticsserviceserver.service.StatisticsIntCounterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.valhallagame.common.rabbitmq.NotificationMessage;
-import com.valhallagame.statisticsserviceserver.service.StatisticsIntCounterService;
-
 public class NotificationConsumer {
-	
+	private static final Logger logger = LoggerFactory.getLogger(NotificationConsumer.class);
+
 	@Autowired
-	StatisticsIntCounterService statisticsService;
+	private StatisticsIntCounterService statisticsService;
 	
 	@RabbitListener(queues = { "#{statisticsCharacterDeleteQueue.name}" })
 	public void receiveCharacterDelete(NotificationMessage message) {
+		logger.info("Received character delete notification with message {}", message);
 		String characterName = (String) message.getData().get("characterName");
 		statisticsService.deleteStatistics(characterName);
 	}

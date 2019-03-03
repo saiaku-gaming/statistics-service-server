d84b8e021c52da1942cec0e3990a068db74208aa
Mio Engel <mioengel@gmail.com>
2019-03-03 23:08:49 +0100

    More MDC fixing

--- src/main/java/com/valhallagame/statisticsserviceserver/service/StatisticsLowTimerService.java
+++ src/main/java/com/valhallagame/statisticsserviceserver/service/StatisticsLowTimerService.java
@@ -5,12 +5,12 @@ import com.valhallagame.characterserviceclient.model.CharacterData;
 import com.valhallagame.common.RestResponse;
 import com.valhallagame.common.rabbitmq.NotificationMessage;
 import com.valhallagame.common.rabbitmq.RabbitMQRouting;
+import com.valhallagame.common.rabbitmq.RabbitSender;
 import com.valhallagame.statisticsserviceclient.message.StatisticsKey;
 import com.valhallagame.statisticsserviceserver.model.StatisticsLowTimer;
 import com.valhallagame.statisticsserviceserver.repository.StatisticsLowTimerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,7 +26,7 @@ public class StatisticsLowTimerService {
 	private StatisticsLowTimerRepository lowTimerRepository;
 
 	@Autowired
-	private RabbitTemplate rabbitTemplate;
+	private RabbitSender rabbitSender;
 	
 	@Autowired
 	private CharacterServiceClient characterServiceClient;
@@ -50,7 +50,7 @@ public class StatisticsLowTimerService {
 
 		logger.info("incremented and now sending");
 
-		rabbitTemplate.convertAndSend(RabbitMQRouting.Exchange.STATISTICS.name(),
+		rabbitSender.sendMessage(RabbitMQRouting.Exchange.STATISTICS,
 				RabbitMQRouting.Statistics.LOW_TIMER.name(),
 				notificationMessage);
 
